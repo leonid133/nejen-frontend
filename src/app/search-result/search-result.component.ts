01568e362426edb4e14f50cb04023ac4c869cfbd
lblokhin <lenin133@yandex.ru>
2018-03-28 12:43:08 +0300

    fix search-result component

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -11,27 +11,7 @@ import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
   selector: 'app-search-result',
   templateUrl: './search-result.component.html'
 })
-export class SearchResultComponent implements OnInit {
-  //heroes$: Observable<Hero[]>;
- // private searchTerms = new Subject<string>();
+export class SearchResultComponent {
 
- // constructor(private heroService: HeroService) {}
-// Push a search term into the observable stream.
- // search(term: string): void {
- //   this.searchTerms.next(term);
- // }
-
-//  ngOnInit(): void {
- //   this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-   //   debounceTime(300),
-
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
- //     switchMap((term: string) => this.heroService.searchHeroes(term)),
- //   );
-  }
 }
 
